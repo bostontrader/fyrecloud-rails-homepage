b96b85fae14d04d4ac036850ba89553c54a5c52d
spartacus <spartacus@unibuck.(none)>
2012-10-06 09:21:41 +0800

    First time commit for the bulk of the application files.

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,39 @@
+class StaticController < ApplicationController
+
+	def android
+		@nav_menu_label = "Amsler"
+	end
+
+	def android
+		@nav_menu_label = "Android"
+	end
+
+	def archaeology
+		@nav_menu_label = "Archaeology"
+	end
+
+	def bitcoins
+		@nav_menu_label = "Bitcoins"
+	end
+
+	def ckwestor
+		@nav_menu_label = "Ckwestor"
+	end
+
+	def contact
+		@nav_menu_label = "Contact us"
+	end
+
+	def index
+		@nav_menu_label = "Home"
+	end
+
+	def rails
+		@nav_menu_label = "Rails"
+	end
+
+	def redstar
+		@nav_menu_label = "Redstar Exchange"
+	end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsapp::Application.config.secret_token = '71b08bf92c02113bccaba3f9b10f05c389f5b8be015b6bc541e6672753f35e9882f60dc550a107162737140625ca5e824da970edae6766d30d812cd3499e3b0c'
